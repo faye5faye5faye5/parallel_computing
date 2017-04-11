e0f13ead7fc93f39ac5e0cccba88c48280268e8f
veganafro <jmuhia80@gmail.com>
2017-04-11 16:28:04 -0400

    building main

--- lab_2/genprime.c
+++ lab_2/genprime.c
@@ -5,4 +5,10 @@
 
 int main(int argc char * argv[]) {
     int N = strtol(argv[1], NULL, 10);
+    int t = strtol(argv[2], NULL, 10);
+
+    if (!N) {
+        printf("Please enter a number greater than 0.");
+        exit(1);
+    }
 }
