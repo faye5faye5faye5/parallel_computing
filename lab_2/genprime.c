a33732d4411dc9fd3967933b2bf521e3820f70f8
veganafro <jmuhia80@gmail.com>
2017-04-12 19:16:48 -0400

    building find_prime

--- lab_2/genprime.c
+++ lab_2/genprime.c
@@ -43,7 +43,7 @@ void find_primes(int n, int t) {
 
     t_start = omp_get_wtime();
 
-#   pragma omp parallel num_threads(t) default(none) shared(prime_num_arr, n) private(loop_index, inner_loop_index)
+#   pragma omp parallel num_threads(t) default(none) shared(prime_num_arr, n) private(outer_loop_index, inner_loop_index)
 #   pragma omp for
     for (outer_loop_index = 4; outer_loop_index < n; outer_loop_index += 1) {
         prime_num_arr[outer_loop_index] = 0;
