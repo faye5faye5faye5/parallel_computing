d594aaa7937924f55bee23d093fadd65fc9259af
veganafro <jmuhia80@gmail.com>
2017-04-14 15:46:09 -0400

    building find_prime

--- lab_2/genprime.c
+++ lab_2/genprime.c
@@ -50,11 +50,12 @@ void find_primes(int n, int t) {
         prime_num_arr[outer_loop_index] = 1;
     }
 
+#   pragma omp for schedule(static, 1)
     for (outer_loop_index = 2; outer_loop_index < sqrt_n; outer_loop_index += 1) {
 
         if (prime_num_arr[outer_loop_index]) {
 
-#           pragma omp for
+// #           pragma omp for
             for (inner_loop_index = outer_loop_index * 2; inner_loop_index < n; inner_loop_index += outer_loop_index) {
                 prime_num_arr[inner_loop_index] = 0;
             }
