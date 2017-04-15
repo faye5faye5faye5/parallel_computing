9a6ebbcb40dca3e2919496f1365156df7ed246c6
veganafro <jmuhia80@gmail.com>
2017-04-15 18:37:09 -0400

    building find_prime

--- lab_2/genprime.c
+++ lab_2/genprime.c
@@ -51,7 +51,7 @@ void find_primes(int n, int t) {
 
     t_start = omp_get_wtime();
 
-    for (outer_loop_index = 2; outer_loop_index < sqrt_n; outer_loop_index += 1) {
+    for (outer_loop_index = 2; outer_loop_index <= sqrt_n; outer_loop_index += 1) {
 
         if (prime_num_arr[outer_loop_index]) {
 
