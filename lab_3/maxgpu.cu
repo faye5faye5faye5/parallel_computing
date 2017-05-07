a8018b1ec13f6e12c6ff4226effb1ff560f7de43
veganafro <jmuhia80@gmail.com>
2017-05-07 17:50:12 -0400

    working on lab 3

--- lab_3/maxgpu.cu
+++ lab_3/maxgpu.cu
@@ -7,7 +7,7 @@
 void getmaxcu(unsigned int * number, unsigned int * maximum, int size);
 
 int main(int argc, char * argv[]) {
-    unsigned int size = 0;
+    unsigned int size;
     unsigned int loop_index;
     unsigned int * number;
 
