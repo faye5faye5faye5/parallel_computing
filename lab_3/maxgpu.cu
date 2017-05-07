b6d005eb9f4fb49aa1a9be30c02f2bd9ab7ee66b
veganafro <jmuhia80@gmail.com>
2017-05-07 16:33:17 -0400

    working on lab 3

--- lab_3/maxgpu.cu
+++ lab_3/maxgpu.cu
@@ -6,5 +6,8 @@
 
 __global__
 void getmaxcu(unsigned int * number, unsigned int * maximum, int size) {
-    
+    extern __shared__ unsigned int data[];
+
+    unsigned int t_id = threadIdx.x;
+    unsigned int index = blockIdx.x * (blockDim.x) + threadIdx.x;
 }
