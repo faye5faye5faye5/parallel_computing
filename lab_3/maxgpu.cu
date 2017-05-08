2f9b828119c1b3fca58ba4dd549e468378254025
veganafro <jmuhia80@gmail.com>
2017-05-07 22:47:31 -0400

    working

--- lab_3/maxgpu.cu
+++ lab_3/maxgpu.cu
@@ -4,6 +4,37 @@
 #include <locale.h>
 #include <cuda.h>
 
+__global__ void getmaxcu(unsigned int * number, unsigned int * maximum, int size) {
+    extern __shared__ unsigned int data[];
+
+    unsigned int t_id = threadIdx.x;
+    unsigned int index = blockIdx.x * (blockDim.x) + threadIdx.x;
+
+    data[t_id] = num[index];
+
+    if (index >= size) {
+        data[t_id] = 0;
+    }
+
+    __syncthreads();
+
+    int loop_index;
+    for (loop_index = blockDim.x / 2; loop_index > 0; loop_index >>= 1) {
+
+        __syncthreads();
+
+        if (t_id < loop_index) {
+            if (data[t_id] < data[t_id + loop_index]) {
+                data[t_id] = data[t_id + loop_index];
+            }
+        }
+    }
+
+    if (t_id == 0) {
+        maximum[blockIdx.x] = data[0];
+    }
+}
+
 int main(int argc, char * argv[]) {
     unsigned int size;
     unsigned int loop_index;
@@ -88,35 +119,4 @@ int main(int argc, char * argv[]) {
     cudaFree(device_max);
     free(number_arr);
     exit(0);
-}
-
-__global__ void getmaxcu(unsigned int * number, unsigned int * maximum, int size) {
-    extern __shared__ unsigned int data[];
-
-    unsigned int t_id = threadIdx.x;
-    unsigned int index = blockIdx.x * (blockDim.x) + threadIdx.x;
-
-    data[t_id] = num[index];
-
-    if (index >= size) {
-        data[t_id] = 0;
-    }
-
-    __syncthreads();
-
-    int loop_index;
-    for (loop_index = blockDim.x / 2; loop_index > 0; loop_index >>= 1) {
-
-        __syncthreads();
-
-        if (t_id < loop_index) {
-            if (data[t_id] < data[t_id + loop_index]) {
-                data[t_id] = data[t_id + loop_index];
-            }
-        }
-    }
-
-    if (t_id == 0) {
-        maximum[blockIdx.x] = data[0];
-    }
 }
